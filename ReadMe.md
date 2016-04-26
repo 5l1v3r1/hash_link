8ffb5cd9effdbb42199a6a0afb3161bf09ab2add
arch4ngel <archangel@arch4ngel.ninja>
2016-04-26 10:02:32 -0400

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+# Quick script to link those 5600 mode hashes
+
+## Dependencies
+
+If bundler isn't installed: ```gem install bundler```
+
+Then:
+
+    cd hash_link
+    bundle install
+
+## Usage
+
+    ruby linker.rb <hash file> <cracked file>
---
+++ linker.rb
@@ -0,0 +1,53 @@
+
+require 'colorize'
+
+if ! ARGV[1] or ! ARGV[2]
+
+	puts "Usage"
+	puts "ruby #{$0} <hashes file> <cracked file>"
+	exit
+
+else
+
+	hashed = {}
+
+	infile = ARGV[1]
+
+	File.open(infile,'r') {|file|
+		file.each do |l|
+			split = l.strip.split(':')
+
+			next if split == []
+
+			hash = split[3]
+			username = split[0].downcase
+
+			if ! hashed[hash]
+				hashed[hash] = [username]
+			else
+				hashed[hash] << username
+			end
+
+		end
+	}
+
+	cracked = {}
+
+	infile = ARGV[2]
+
+	File.open(infile, 'r') {|file|
+		file.each do |l| 
+			split = l.strip.split(':')
+
+			next if split == []
+			
+			split[1] = '<< BLANK / NO PASSWORD >>' if ! split[1]
+
+			hash = split[0]
+			password = split[1]
+
+			puts password.green() + ": " + hashed[hash].join(', ')
+		end
+	}
+
+end
